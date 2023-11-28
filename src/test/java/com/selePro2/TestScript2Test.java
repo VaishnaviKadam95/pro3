6659c54275e5251cc3f43fdf4daf3751c58da42d
Dell <Dell@DESKTOP-NQP4P5N>
2023-11-28 11:23:58 +0530

    Framework is done

---
+++ src/test/java/com/selePro2/TestScript2Test.java
@@ -0,0 +1,24 @@
+package com.selePro2;
+
+import org.testng.annotations.Test;
+
+public class TestScript2Test {
+	public class TestScript1Test {
+		@Test(groups="smoke")
+	public void ts1() {
+		System.out.println("==TestScript 1===");
+	}
+		@Test
+	public void ts2() {
+		System.out.println("==TestScript 2===");
+	}
+		@Test
+	public void ts3() {
+		System.out.println("==TestScript 3===");
+	}
+		@Test
+		public void ts4() {
+			System.out.println("==TestScript 4===");
+		}
+}
+}
